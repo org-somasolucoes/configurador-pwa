eecb29f2748827131f51b79b56655c2e2934d2b0
RafaelNatsu <rafaelnatsu99@gmail.com>
2023-09-08 15:28:51 -0300

    chore: Adicionado biblioteca de cor em string chat
    Para melhor visualização das informações foi adicionado cores no chat

--- src/class/PWA.js
+++ src/class/PWA.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database/pwadb.db');
-
+const colors = require('colors');
 class PWA {
     //TODO: a ideia aki é ter algo q se um dia pedir pra alterar de tranquilo de forma facil
     /**
@@ -52,35 +52,35 @@ class PWA {
      *  deve se ao inserir a função pedir as informações para criar o pwa
      */
     async setPWA() {
-        this.chat.say('Insira as informações necessárias');
-        const localInstalação = await this.chat.question("Local de instalação do pwa (exemplo:/usuario/pasta/do/projeto/)");
-        const nome = await this.chat.question("Nome para a aplicação");
-        const nomeResumido = await this.chat.question("Nome abreviado");
-        const descricao = await this.chat.question("Adicione uma descrição");
-        const corTema = await this.chat.question("Adicione uma cor em hexadecimal (exemplo:#123123)");
+        this.chat.say('Insira as informações necessárias'.green);
+        const localInstalação = await this.chat.question("Local de instalação do pwa ".green+"(exemplo:/usuario/pasta/do/projeto/)".red);
+        const nome = await this.chat.question("Nome para a aplicação".green);
+        const nomeResumido = await this.chat.question("Nome abreviado".green);
+        const descricao = await this.chat.question("Adicione uma descrição".green);
+        const corTema = await this.chat.question("Adicione uma cor em hexadecimal (exemplo:#123123)".green);
 
         //TODO: implementar depois a possibilidade de adicionar N imagens 
 
-        const iconeUrlPwa1 = await this.chat.question("Local da onde a imagem de PWA1 esta visivel: (exemplo: https://dominioExemplo.com/assets/imagems/nome_imagem.png)");
-        this.chat.say('Lembre-se :>> ', );
-        this.chat.say("Adicione uma imagem para o icone que DEVE ser quadradas, no padrão aceitavel e suas informações validas");
-        const iconeTamanhoPwa1 = await this.chat.question("Qual é o tamanho da imagem PWA1? (exemplo: 512x512)");
-        const iconeTipoPwa1 = await this.chat.question("Qual é o tipo da imagem PWA1? (exemplo: image/png)");
+        const iconeUrlPwa1 = await this.chat.question("Local da onde a imagem de PWA1 esta visivel:".green+" (exemplo: https://dominioExemplo.com/assets/imagems/nome_imagem.png)".red);
+        this.chat.say('Lembre-se :>> '.yellow);
+        this.chat.say("Adicione uma imagem para o icone que DEVE ser quadradas, no padrão aceitavel e suas informações validas".yellow);
+        const iconeTamanhoPwa1 = await this.chat.question("Qual é o tamanho da imagem PWA1?".green+" (exemplo: 512x512)".red);
+        const iconeTipoPwa1 = await this.chat.question("Qual é o tipo da imagem PWA1?".green+" (exemplo: image/png)".red);
         
 
         //* const addPWA = require('./addPwa.js');
-        this.chat.say('\nAdicionar um novo PWA: ');
-        this.chat.say('Local:'+ localInstalação)
-        this.chat.say('Nome do PWA: '+nome);
-        this.chat.say('Nome resumido do PWA: '+nomeResumido);
-        this.chat.say('Descrição do PWA: '+descricao);
-        this.chat.say('Cor tema do PWA: '+corTema);
+        this.chat.say('\nAdicionar um novo PWA: '.yellow);
+        this.chat.say('Local: '.yellow+ localInstalação)
+        this.chat.say('Nome do PWA: '.yellow+nome);
+        this.chat.say('Nome resumido do PWA: '.yellow+nomeResumido);
+        this.chat.say('Descrição do PWA: '.yellow+descricao);
+        this.chat.say('Cor tema do PWA: '.yellow+corTema);
 
-        this.chat.say('Url local da imagem: '+iconeUrlPwa1);
-        this.chat.say('Tamanho do arquivo da imagem do ícone1: '+iconeTamanhoPwa1);
-        this.chat.say('Tipo do icone1: '+iconeTipoPwa1);
+        this.chat.say('Url local da imagem: '.yellow+iconeUrlPwa1);
+        this.chat.say('Tamanho do arquivo da imagem do ícone1: '.yellow+iconeTamanhoPwa1);
+        this.chat.say('Tipo do icone1: '.yellow+iconeTipoPwa1);
 
-        const confirmaAdiciona = await this.chat.question("Confirma?(Y/n)");
+        const confirmaAdiciona = await this.chat.question("Confirma?("+"Y".green+"/"+"n".red+")");
         if(confirmaAdiciona.toLowerCase() != 'n'){
             const insertQuery = 
             `INSERT INTO pwas (localInstalacao, nome, nomeResumido, descricao, cor, iconeUrl1, iconeTamanho1, iconeTipoImagem1)
@@ -92,12 +92,12 @@ class PWA {
                         throw new Error('Erro ao adicionar o PWA '+err);
                     }
                 });
-                this.chat.say("PWA adicionado com sucesso!");
+                this.chat.say("PWA adicionado com sucesso!".green);
             } catch (error) {
                 console.error(error);
             }
         }
-        else this.chat.say("Cancelado.");
+        else this.chat.say("Cancelado.".red);
         
     }
 
@@ -118,10 +118,10 @@ class PWA {
                         if (rows.length === 0) {
                             reject(err)
                         } else {
-                            console.log('Lista de PWAs:');
+                            console.log('Lista de PWAs:'.yellow);
                             rows.forEach((row) => {
                                 //foi feito assim para de forma provisória para ser generico
-                                console.log(`registro ${row.id}: ${JSON.stringify(row)}`);
+                                console.log(`registro ${String(row.id).yellow}: ${JSON.stringify(row)}`);
                             });
                             resolve();
                         }
@@ -196,10 +196,10 @@ class PWA {
 
 
     async deletePWa(){
-        this.chat.say('Lista de todos os registros');
+        this.chat.say('Lista de todos os registros'.yellow);
         await this.viewPwa();
-        const id = await this.chat.question('Digite um Id para deletar');
-        const confirmacao = await this.chat.question('Confirma a exclusão? (y/N)');
+        const id = await this.chat.question('Digite um Id para deletar'.red);
+        const confirmacao = await this.chat.question("Confirma a exclusão? ("+"y".green+"/"+"N".red+")");
         if (confirmacao.toLowerCase() == 'y') {
             const __id = Number(id);
             const deleteSql = "DELETE FROM pwas WHERE id = "+__id;
@@ -211,12 +211,12 @@ class PWA {
                     })
                 })
                 if(pwaDeleted == 'err') throw new Error("Erro ao tentar deletar acessar o id: "+__id);
-                this.chat.say('Registro excluído com sucesso.');
+                this.chat.say('Registro excluído com sucesso.'.green);
             } catch (error) {
                 console.error(error.message);
             }
         } else {
-            this.chat.say('Exclusão cancelada.');
+            this.chat.say('Exclusão cancelada.'.red);
         }
     }
 
--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ const PWA = require('./class/PWA');
 const ImprimePwa = require('./class/Imprime');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database/pwadb.db');
-
+const colors = require('colors');
 /**
  * Criar alguma forma q seja igual a classe PWA q será criado
  */
 
 function menu(chat) {
     // return new Promise((resolve, reject) => {
-        chat.say('----------------------------------------------------------------');
-        chat.say('\n--Menu--\n');
-        chat.say('Adicionar: Para adicionar um PWA');
-        chat.say('Visualizar: Para visualizar a lista de todos os pwa');
-        chat.say('Deletar: Para deletar um PWA');
-        chat.say('Atualizar: Para atualizar PWAs');
-        chat.say('Sair: Para sair');
-        chat.say('\nEscolha uma das opções acima\n----------------------------------------------------------------');
+        chat.say('----------------------------------------------------------------'.green);
+        chat.say('\n--Menu--\n'.rainbow);
+        chat.say('Adicionar:'.green+' Para adicionar um PWA');
+        chat.say('Visualizar:'.yellow+' Para visualizar a lista de todos os pwa');
+        chat.say('Deletar:'.red+' Para deletar um PWA');
+        chat.say('Atualizar:'.blue+' Para atualizar PWAs');
+        chat.say('Sair:'.red+' Para sair');
+        chat.say('----------------------------------------------------------------\nEscolha uma das opções acima\n'.green);
     // })
 }
 
 const main = async () => {
     const chat = new Chat();
-    chat.say("Bem-vindo ao configurador de pwa! Digite 'sair' para sair.");
+    chat.say("Bem-vindo ao configurador de pwa! Digite 'sair' para sair.".green);
     const pwa = new PWA(chat);
     const imprime = new ImprimePwa();
     let sair = false;
@@ -34,7 +34,7 @@ const main = async () => {
 
     while (!sair) {
         await menu(chat);
-        let message = await chat.question("Digite uma opção");
+        let message = await chat.question("Digite uma opção".bgGreen);
         switch (message && message.toLowerCase()) {
             case 'adicionar':
                 await pwa.setPWA();
@@ -44,11 +44,11 @@ const main = async () => {
                 break;
             case 'atualizar':
                 await pwa.viewPwa();
-                const id = await chat.question('Selecione um id para imprimir');
+                const id = await chat.question('Selecione um id para imprimir'.yellow);
                 const obj = await pwa.getFormattedPwa(id);
-                chat.say('Preview');
+                chat.say('Preview'.yellow);
                 chat.say(obj.PWA);
-                const confirmacao = await chat.question('Confirma a escrita do arquivo? (Y/n)');
+                const confirmacao = await chat.question("Confirma a escrita do arquivo? ("+"Y".green+"/"+"n".red+")");
                 if(confirmacao != 'n') imprime.imprimerPwa(obj.local , obj.PWA);
                 else chat.say('Cancelado!');
                 break;
@@ -63,7 +63,7 @@ const main = async () => {
                 break;
         }
     }
-    chat.say("Obrigado por usar o configurador de pwa. Até logo!");
+    chat.say("Obrigado por usar o configurador de pwa. Até logo!".rainbow);
     process.exit(0);
 }
 
